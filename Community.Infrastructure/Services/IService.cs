3cca52f64db85b6e26d6c234c2e926efc71983a4
chyla95 <chyla95@gmail.com>
2022-12-21 04:07:23 +0100

    Enable tracking by default

--- Community.Infrastructure/Services/IService.cs
+++ Community.Infrastructure/Services/IService.cs
@@ -4,8 +4,8 @@ namespace Community.Infrastructure.Services
 {
     public interface IService <T> where T : Entity
     {
-        Task<IEnumerable<T>> GetManyAsync(bool isTrackingEnabled = false);
-        Task<T?> GetAsync(int id, bool isTrackingEnabled = false);
+        Task<IEnumerable<T>> GetManyAsync(bool isTrackingEnabled = true);
+        Task<T?> GetAsync(int id, bool isTrackingEnabled = true);
         Task AddAsync(T entity);
         Task UpdateAsync(T entity);
         Task RemoveAsync(T entity);
